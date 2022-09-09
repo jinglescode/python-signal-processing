bfb7a07f724c01ff1b7e60906ed42276df5baa93
Hong Jing (Jingles) <xiao_laoshu@hotmail.com>
2022-09-09 13:17:37 +0800

    Update __init__.py

--- splearn/data/__init__.py
+++ splearn/data/__init__.py
@@ -2,7 +2,6 @@ from .pytorch_dataset import PyTorchDataset, PyTorchDataset2Views
 from .multiple_subjects import MultipleSubjects
 from .benchmark import Benchmark
 from .openbmi import OPENBMI
-from .visual_field import VisualField
 from .sample_ssvep import SampleSSVEPData
 
 from .generate import generate_signal
